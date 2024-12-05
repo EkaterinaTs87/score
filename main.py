bd36c70ab1f32f65c13fd80ef089f213b724725c
tsege <oep87@mail.ru>
2024-12-05 15:18:09 +0800

    изменила под песню)

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-ball_team1 = 3
-ball_team2 = 2
+ball_Argentina = 5
+ball_Jamaica = 0
 
 
-def score_ball(team1, team2):
-    if team1 < 0 or team2 < 0:
-        return "Количество мячей не может быть отрицательным."
+def score_ball(Argentina, Jamaica):
+    if Argentina < 0 or Jamaica < 0:
+        return "Количество мячей не может быть отрицательным!"
 
-    if team1 > team2:
-        result = f"Команда1 выиграла со счетом {team1} : {team2}."
-    elif team2 > team1:
-        result = f"Команда2 выиграла со счетом {team2} : {team1}."
+    if Argentina > Jamaica:
+        result = f"Какая боль! Какая боль! Аргентина - Ямайка {Argentina} : {Jamaica}!"
+    elif Jamaica > Argentina:
+        result = f"Ямайка выиграла со счётом {Jamaica} : {Argentina}"
     else:
-        result = f"Ничья со счетом {team1} : {team2}."
+        result = f"Ничья со счётом {Argentina} : {Jamaica}"
 
     return result
 
 
-result = score_ball(ball_team1, ball_team2)
+result = score_ball(ball_Argentina, ball_Jamaica)
 print(result)
